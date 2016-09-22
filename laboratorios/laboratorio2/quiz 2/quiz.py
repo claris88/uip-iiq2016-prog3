eb6c9b9bb02fb1372f98316fa61fe3e6b17803a8
Gerardo <gerardochial@hotmail.com>
2016-09-21 22:00:06 -0500

    `proyecto2 conversion de moneda

---
+++ laboratorios/laboratorio2/quiz 2/quiz.py
@@ -0,0 +1,18 @@
+print("conversion de moneda":)
+
+dolares = centavos / 100
+
+eur = dolares*0.8965
+yen = dolares * 101.5744
+bp = dolares * 0.7702
+mix = dolares * 19.7843
+
+print ("la cantidad a convertir", + srt("centavos = dolares")
+
+print (" la cantidad a convertir", + str("dolares = euros")
+
+print (" la cantidad a convertir", + str("euros = yen")
+
+print (" la cantidad a convertir", + str("yen = bp")
+
+print (" la cantidad a convertir", + str("bp = mix")
