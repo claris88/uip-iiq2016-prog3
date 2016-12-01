1935f2c7d3d7f42746201a9ddc8791a3f7a08bdd
claris88 <claris-688@hotmail.com>
2016-11-30 21:51:47 -0500

    laboratorio7
    nbm

---
+++ laboratorios/laboratorio7/juanpregunta.py
@@ -0,0 +1,18 @@
+while true:
+  preg = input ("preguntale a juan:")
+
+if preg.ednwith (´?´):
+
+print("ofi")
+
+elif preg>= Áánd preg <=´z´:
+
+print("chilea")
+
+elif (len(preg)==0):
+
+print ("mm")
+
+
+else:
+print ("me da igual")
